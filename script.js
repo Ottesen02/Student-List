497ceb2c9bbd54d40cd66e14256557140f4c9157
Martin Ottesen <martinottesen@Martins-MacBook-Pro-2.local>
2019-09-04 20:57:19 +0200

    Json loaded and fitering working

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+window.addEventListener("DOMContentLoaded", start);
+
+function start() {
+  console.log("Let the script begin");
+  getJson();
+}
+
 let students = [];
-document.addEventListener("DOMContentLoaded", getJson);
+let dest = document.querySelector(".students");
+let temp = document.querySelector(".temp");
+let filterHouse = "all";
+
 async function getJson() {
   const url = "http://petlatkea.dk/2019/students1991.json";
   const studentsJson = await fetch(url);
@@ -9,14 +19,34 @@ async function getJson() {
 }
 
 function showStudents() {
-  let dest = document.querySelector(".students");
-  let temp = document.querySelector(".temp");
-
+  dest.innerHTML = "";
   students.forEach(student => {
-    let klon = temp.cloneNode(true).content;
-    klon.querySelector("h2").innerHTML = student.fullname;
-    klon.querySelector("p").innerHTML = student.house;
+    console.log(filterHouse);
+    if (filterHouse == "all" || filterHouse == student.house) {
+      let klon = temp.cloneNode(true).content;
+      klon.querySelector("h2").innerHTML = student.fullname;
+      klon.querySelector("p").innerHTML = student.house;
 
-    dest.appendChild(klon);
+      document.querySelector(".firstname").addEventListener("click", () => {
+        student.sort((a, z) => {
+          return a.fullname.localeCompare(z.fullname);
+        });
+      });
+
+      dest.appendChild(klon);
+    }
   });
+}
+
+document.querySelectorAll(".filter").forEach(but => {
+  but.addEventListener("click", filtrering);
+});
+
+function filtrering() {
+  document.querySelectorAll(".filter").forEach(but => {
+    but.classList.remove("valgt");
+  });
+  this.classList.add("valgt");
+  filterHouse = this.getAttribute("data-house");
+  showStudents();
 }
